2ac31dc9d91ecd152fe127c04dc34977c26edd14
Alice Knag <1855905+AskAlice@users.noreply.github.com>
2021-06-06 16:56:05 +0000

    sectionctl: add 1.11.1 bottle.

--- Formula/sectionctl.rb
+++ Formula/sectionctl.rb
@@ -5,6 +5,13 @@ class Sectionctl < Formula
   sha256 "5d3b47ea1b2641aa312e13a8bd282cb3aeb7caa540585e8f600dd621a3b1bfda"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/section/homebrew-brews/releases/download/sectionctl-1.11.1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, catalina:     "4584c4ca6c5d16d8ec8c4b21e1b739b99f9a3cd65c2e33f88acd60122b98fce8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "8458a4a2d15729f8cb2010687d7fdbcd8adb11bd4edc1ad4f5c4b28e908759d9"
+  end
+
   depends_on "go" => :build
 
   def install
