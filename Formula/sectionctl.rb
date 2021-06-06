db9c6d518cbf9c888038e1dcedd50318d852eda9
AskAlice <alice@section.io>
2021-06-06 10:25:42 -0600

    update

--- Formula/sectionctl.rb
+++ Formula/sectionctl.rb
@@ -1,6 +1,6 @@
 class Sectionctl < Formula
+  desc "Section Command-line Tools"
   version "1.11.1"
-  desc "sectionctl Commandline Tools"
   homepage "https://github.com/section/sectionctl"
   url "https://github.com/section/sectionctl/archive/refs/tags/v#{version}.tar.gz"
   sha256 "5d3b47ea1b2641aa312e13a8bd282cb3aeb7caa540585e8f600dd621a3b1bfda"
